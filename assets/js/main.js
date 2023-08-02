04d07726b2e83768ec88f8fae6172a9d5ace2fe2
Napoleon Anaya R <napoleonanaya@gmail.com>
2023-08-01 22:55:40 -0500

    feat: adds to main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,8 @@
 const navMenu = document.getElementById('nav-menu'),
   navToggle = document.getElementById('nav-toggle'),
-  navClose = document.getElementById('nav-close')
-
+  navClose = document.getElementById('nav-close'),
+  navLink = document.querySelectorAll('.nav__link')
+  
   if(navToggle){
     navToggle.addEventListener('click', () =>{
       navMenu.classList.add('show-menu')
@@ -13,3 +14,22 @@ const navMenu = document.getElementById('nav-menu'),
       navMenu.classList.remove('show-menu')
     })
   }
+
+  function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+  }
+  navLink.forEach(n => n.addEventListener('click', linkAction))
+
+  //Scroll reveal animation
+  const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    reset: true
+  })
+  sr.reveal(`.home-swiper, .new-swiper, .newsletter__container`)
+  sr.reveal(`.category__data, .trick__content, .footer__content`, {interval: 100})
+  sr.reveal(`.about__data, .discount__img`, {origin: 'left'})
+  sr.reveal(`.about__img, .discount__data`, {origin: 'right'})
