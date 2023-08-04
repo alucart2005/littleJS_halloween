3aee9b71da999cea39ac554634243647d2ff33c1
Napoleon Anaya R <napoleonanaya@gmail.com>
2023-08-03 22:42:46 -0500

    feat: end project

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,6 +20,7 @@ const navMenu = document.getElementById('nav-menu'),
     navMenu.classList.remove('show-menu')
   }
   navLink.forEach(n => n.addEventListener('click', linkAction))
+
   //SWIPER
   let homeSwiper = new Swiper(".home-swiper", {
     spaceBetween: 30,
@@ -28,15 +29,50 @@ const navMenu = document.getElementById('nav-menu'),
       el: ".swiper-pagination",
       clickable: true,
     },
-
   })
-  //Scroll reveal animation
+  let newSwiper = new Swiper(".new-swiper", {
+    centeredSlides: true,
+    slidesPerView: "auto",
+    loop: true,
+    spaceBetween: 16,
+  });
+
+  //SCROLL up
+  function scrollUp(){
+    const scrollUp = document.getElementById('scroll-up');
+    if(this.scrollY >= 460) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+  };
+  window.addEventListener('scroll', scrollUp)
+
+  //SECTIONS ACTIVE LINK
+  const sections = document.querySelectorAll('section[id]')
+  function scrollActive(){
+    const scrollY = window.scrollY
+    sections.forEach(current =>{
+      const sectionHeight = current.offsetHeight, 
+        sectionTop = current.offsetTop - 58, 
+        sectionId = current.getAttribute('id')
+        console.log("scrollY " + scrollY)
+        console.log("sectionId " + sectionId)
+        console.log("sectionTop " + sectionTop)
+        console.log("sectionHeight " + sectionHeight)
+      if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+        document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+      } else{
+        document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        console.log("sectionId " + sectionId) 
+      }
+    })
+  };
+  window.addEventListener('scroll', scrollActive)
+
+  //Scroll reveal REVEAL ANIMATION
   const sr = ScrollReveal({
     origin: 'top',
     distance: '60px',
     duration: 2500,
     delay: 400,
-    reset: true
+    //reset: true
   })
   sr.reveal(`.home-swiper, .new-swiper, .newsletter__container`)
   sr.reveal(`.category__data, .trick__content, .footer__content`, {interval: 100})
