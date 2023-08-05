1eccd6f91ddb15072556daba9e0ba6c6acb2143b
Napoleon Anaya R <napoleonanaya@gmail.com>
2023-08-05 18:42:07 -0500

    feat: delete the console.log in main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,44 +1,39 @@
-const navMenu = document.getElementById('nav-menu'),
-  navToggle = document.getElementById('nav-toggle'),
-  navClose = document.getElementById('nav-close'),
-  navLink = document.querySelectorAll('.nav__link')
-  
-  if(navToggle){
-    navToggle.addEventListener('click', () =>{
-      navMenu.classList.add('show-menu')
-    })
-  }
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close"),
+  navLink = document.querySelectorAll(".nav__link");
 
-  if(navClose){
-    navClose.addEventListener('click', () =>{
-      navMenu.classList.remove('show-menu')
-    })
-  }
-
-  function linkAction(){
-    const navMenu = document.getElementById('nav-menu')
-    navMenu.classList.remove('show-menu')
-  }
-  navLink.forEach(n => n.addEventListener('click', linkAction))
-
-  //SWIPER
-  let homeSwiper = new Swiper(".home-swiper", {
-    spaceBetween: 30,
-    loop: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-  })
-
-
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
+function linkAction() {
+  const navMenu = document.getElementById("nav-menu");
+  navMenu.classList.remove("show-menu");
+}
+navLink.forEach((n) => n.addEventListener("click", linkAction));
+//SWIPER
+let homeSwiper = new Swiper(".home-swiper", {
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
 function createSwiper(selector) {
   return new Swiper(selector, {
-    effect: 'coverflow',
+    effect: "coverflow",
     loop: true,
     grabCursor: true,
     centeredSlides: true,
-    slidesPerView: 'auto',
+    slidesPerView: "auto",
     coverflowEffect: {
       rotate: 10,
       stretch: 0,
@@ -47,7 +42,7 @@ function createSwiper(selector) {
       slideShadows: true,
     },
     pagination: {
-      el: '.swiper-pagination',
+      el: ".swiper-pagination",
       clickable: true,
     },
     autoplay: {
@@ -56,56 +51,47 @@ function createSwiper(selector) {
     },
   });
 }
-
 let newSwiper = createSwiper(".new-swiper");
 let trick = createSwiper(".swiper-trick");
-
-
-
-
-
-
-
-
-
-  //SCROLL up
-  function scrollUp(){
-    const scrollUp = document.getElementById('scroll-up');
-    if(this.scrollY >= 460) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
-  };
-  window.addEventListener('scroll', scrollUp)
-
-  //SECTIONS ACTIVE LINK
-  const sections = document.querySelectorAll('section[id]')
-  function scrollActive(){
-    const scrollY = window.scrollY
-    sections.forEach(current =>{
-      const sectionHeight = current.offsetHeight, 
-        sectionTop = current.offsetTop - 58, 
-        sectionId = current.getAttribute('id')
-        console.log("scrollY " + scrollY)
-        console.log("sectionId " + sectionId)
-        console.log("sectionTop " + sectionTop)
-        console.log("sectionHeight " + sectionHeight)
-      if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-        document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
-      } else{
-        document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
-        console.log("sectionId " + sectionId) 
-      }
-    })
-  };
-  window.addEventListener('scroll', scrollActive)
-
-  //Scroll reveal REVEAL ANIMATION
-  const sr = ScrollReveal({
-    origin: 'top',
-    distance: '60px',
-    duration: 2500,
-    delay: 400,
-    //reset: true
-  })
-  sr.reveal(`.home-swiper, .new-swiper, .newsletter__container`)
-  sr.reveal(`.category__data, .trick__content, .footer__content`, {interval: 100})
-  sr.reveal(`.about__data, .discount__img`, {origin: 'left'})
-  sr.reveal(`.about__img, .discount__data`, {origin: 'right'})
+//SCROLL up
+function scrollUp() {
+  const scrollUp = document.getElementById("scroll-up");
+  if (this.scrollY >= 460) scrollUp.classList.add("show-scroll");
+  else scrollUp.classList.remove("show-scroll");
+}
+window.addEventListener("scroll", scrollUp);
+//SECTIONS ACTIVE LINK
+const sections = document.querySelectorAll("section[id]");
+function scrollActive() {
+  const scrollY = window.scrollY;
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 58,
+      sectionId = current.getAttribute("id");
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.add("active-link");
+    } else {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.remove("active-link");
+      console.log("sectionId " + sectionId);
+    }
+  });
+}
+window.addEventListener("scroll", scrollActive);
+//Scroll reveal REVEAL ANIMATION
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "60px",
+  duration: 2500,
+  delay: 400,
+  //reset: true
+});
+sr.reveal(`.home-swiper, .new-swiper, .newsletter__container`);
+sr.reveal(`.category__data, .trick__content, .footer__content`, {
+  interval: 100,
+});
+sr.reveal(`.about__data, .discount__img`, { origin: "left" });
+sr.reveal(`.about__img, .discount__data`, { origin: "right" });
